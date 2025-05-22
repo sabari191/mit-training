924852b85f15b195fb2bdd61f81db9fd5877ac78
sabari <sabarinath.v@mastechdigital.com>
2025-05-22 16:26:54 +0530

    Wether Agent

--- WeatherAgent.py
+++ WeatherAgent.py
@@ -1,9 +1,10 @@
 # WeatherAgent.py
 
 from BaseAgent import BaseAgent
+import random
 
 def weather_action(location: str):
-    return f"The current weather in {location} is Sunny with 25°C temperature."
+    return f"The current weather in {location} is Sunny with {random.randint(20,45)} degree fareheit temperature."
 
 class WeatherAgent(BaseAgent):
     def __init__(self):
